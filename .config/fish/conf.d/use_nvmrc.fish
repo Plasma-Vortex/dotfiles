034eb18fdd66ea28b0fb9edb2b4fcb1b3b59a0ed
Howard Halim <howardhalim@gmail.com>
2023-01-03 20:22:57 -0500

    use_nvmrc

---
+++ .config/fish/conf.d/use_nvmrc.fish
@@ -0,0 +1,10 @@
+function use_nvmrc --on-variable PWD
+    if not status --is-interactive
+        return
+    end
+    if status --is-command-substitution
+        return
+    end
+
+    nvm use > /dev/null
+end
