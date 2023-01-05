5fd9b1299879b4142bdba1d481102872b17d08cc
Howard Halim <howardhalim@gmail.com>
2023-01-05 03:18:05 -0500

    helix config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
 
-    set -gx EDITOR nvim
+    set -gx EDITOR helix
 
     set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 end
