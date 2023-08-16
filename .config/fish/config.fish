715fabba7824ec5bf7a04693d05e200d5b810034
Howard Halim <howardhalim@gmail.com>
2023-08-16 02:28:17 -0400

    no more miniconda or pdm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,16 +6,10 @@ if status is-interactive
 
     set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
-    if test -n "$PYTHONPATH"
-        set -x PYTHONPATH '/usr/lib/python3.10/site-packages/pdm/pep582' $PYTHONPATH
-    else
-        set -x PYTHONPATH '/usr/lib/python3.10/site-packages/pdm/pep582'
-    end
-
     # >>> conda initialize >>>
     # !! Contents within this block are managed by 'conda init' !!
-    if test -f /home/howard/miniconda3/bin/conda
-        eval /home/howard/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-    end
+    # if test -f /home/howard/miniconda3/bin/conda
+    #     eval /home/howard/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    # end
     # <<< conda initialize <<<
 end
